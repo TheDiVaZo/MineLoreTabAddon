b0e10fdfc05af9c47f1a3be63ba7ed7543ec9f15
TheDiVaZo (ronad) <thedivazoyoutub@gmail.com>
2025-02-02 22:40:43 +0300

    Инициализация проекта

--- src/main/java/org/minelore/plugin/tabaddon/TabAddon.java
+++ src/main/java/org/minelore/plugin/tabaddon/TabAddon.java
@@ -1,5 +1,8 @@
 package org.minelore.plugin.tabaddon;
 
+import me.neznamy.tab.api.TabAPI;
+import me.neznamy.tab.api.TabPlayer;
+import me.neznamy.tab.api.event.player.PlayerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -11,6 +14,10 @@ public class TabAddon extends JavaPlugin {
     public void onEnable() {
         super.onEnable();
         LOG.info("plugin has enabled. Development by TheDiVaZo for MineLore");
+        TabAPI.getInstance().getEventBus().register(PlayerLoadEvent.class, event -> {
+            TabPlayer tabPlayer = event.getPlayer();
+            //do something
+        });
     }
 
     @Override
